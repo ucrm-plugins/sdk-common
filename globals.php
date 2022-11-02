44d4c599feda67bac6b1755a900506d764f2b38f
Ryan Spaeth <rspaeth@spaethtech.com>
2022-11-02 13:13:32 -0500

    Updates

--- globals.php
+++ globals.php
@@ -1,9 +1,10 @@
-<?php /** @noinspection PhpUnused, SpellCheckingInspection */
+<?php
+
 declare(strict_types=1);
 
 /**
- * @copyright   2022 Spaeth Technologies Inc.
- * @author      Ryan Spaeth (rspaeth@spaethtech.com)
+ * @copyright 2022 Spaeth Technologies Inc.
+ * @author Ryan Spaeth (rspaeth@spaethtech.com)
  *
  * globals.php
  *
@@ -11,72 +12,54 @@ declare(strict_types=1);
  *
  * IMPORTANT: Many of my libraries depend on these constants, so be mindful of what you're doing when editing this file!
  *
+ * cspell:ignore cpuset
  */
 
-#region CONTAINER
+if (!defined("CONTAINER_ID"))
+    define("CONTAINER_ID", PHP_OS !== "WINNT" ? exec("cat /proc/1/cpuset | cut -c9-") : "");
 
-if ( !defined( "__CONTAINER_ID__" ) )
-    define( "__CONTAINER_ID__", PHP_OS !== "WINNT" ? exec( "cat /proc/1/cpuset | cut -c9-" ) : "" );
-
-#endregion
-
-#region DEPLOYMENT
-
-if( !defined( "__DEPLOYMENT__" ) )
-{
+if (!defined("DEPLOYMENT")) {
     class Deployment
     {
         public const REMOTE = "REMOTE";
         public const LOCAL = "LOCAL";
     }
-    
-    define( "__DEPLOYMENT__", ( strpos( __CONTAINER_ID__, exec( "echo \$HOSTNAME" ) ) === 0 ) ? "REMOTE" : "LOCAL" );
+
+    define("DEPLOYMENT", (strpos(CONTAINER_ID, exec("echo \$HOSTNAME")) === 0) ? "REMOTE" : "LOCAL");
 }
 
-#endregion
-
-if( !defined( "__UCRM_VERSION__" ) )
-{
+if (!defined("UCRM_VERSION")) {
     $path = "/usr/src/ucrm/app/config/version.yml";
-    define("__UCRM_VERSION__", file_exists($path) ? yaml_parse_file($path)["version"] : "" );
+
+    define("UCRM_VERSION", file_exists($path) ? yaml_parse_file($path)["version"] : "");
 }
 
 
-#region PROJECT
-
-if (!defined("__PROJECT_DIR__"))
-{
-    define("__PROJECT_DIR__", realpath(__DIR__."/../../../../../"));
+if (!defined("PROJECT_DIR")) {
+    define("PROJECT_DIR", realpath(__DIR__ . "/../../../../../"));
 }
 
-if (!defined("__PROJECT_NAME__"))
-{
-    $path = realpath( __PROJECT_DIR__."/composer.json" );
-    
-    $name = $path ? json_decode(file_get_contents($path), TRUE)["name"] : basename(__PROJECT_DIR__);
-    
+if (!defined("PROJECT_NAME")) {
+    $path = realpath(PROJECT_DIR . "/composer.json");
+
+    $name = $path ? json_decode(file_get_contents($path), TRUE)["name"] : basename(PROJECT_DIR);
+
     $name = strpos($name, "/") !== false ? explode("/", $name)[1] : $name;
-    
-    define("__PROJECT_NAME__", $name);
+
+    define("PROJECT_NAME", $name);
 }
 
 
-#endregion
-
-#region PLUGIN
-
-if (!defined("__PLUGIN_DIR__"))
-{
-    $path = __PROJECT_DIR__ . (__DEPLOYMENT__ === DEPLOYMENT::REMOTE ? "" : "/src");
-    define("__PLUGIN_DIR__", realpath($path));
+if (!defined("PLUGIN_DIR")) {
+    $path = PROJECT_DIR . (DEPLOYMENT === DEPLOYMENT::REMOTE ? "" : "/src");
+    define("PLUGIN_DIR", realpath($path));
 }
 
-if (!defined("__PLUGIN_NAME__"))
-{
-    define("__PLUGIN_NAME__", file_exists(__PLUGIN_DIR__."/manifest.json")
-        ? json_decode(file_get_contents(__PLUGIN_DIR__."/manifest.json"), TRUE)["information"]["name"]
-        : __PROJECT_NAME__
+if (!defined("PLUGIN_NAME")) {
+    define(
+        "PLUGIN_NAME",
+        file_exists(PLUGIN_DIR . "/manifest.json")
+        ? json_decode(file_get_contents(PLUGIN_DIR . "/manifest.json"), TRUE)["information"]["name"]
+        : PROJECT_NAME
     );
 }
-#endregion
-
